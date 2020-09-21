62db2d24822d0d246e90e75d69fa70f00863dfdb
parshuram-patil <parshuram.patil@outlook.in>
2020-09-21 13:17:00 +0530

    reading and writing files to S3 $Env:AWS_PROFILE = "sandbox" $Env:IS_RUNNING_ON_CLOUD = "True" $Env:OUTPUT_PATH = "psp-log-bucket" $Env:TITANIC_DATA_PATH = "psp-data-bucket" $Env:TITANIC_CSV_DATA_FILE = "titanic.csv"

--- config.py
+++ config.py
@@ -20,9 +20,15 @@ class Config:
     __ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 
     # Data files path
-    __TITANIC_DATA_PATH = get_value('TITANIC_DATA_PATH', __ROOT_PATH + '/datasets/titanic/')
-    TITANIC_CSV_DATA_FILE = __TITANIC_DATA_PATH + 'titanic.csv'
-    TITANIC_XLSX_DATA_FILE = __TITANIC_DATA_PATH + 'titanic.xlsx'
-
-    OUTPUT_PATH = get_value('OUTPUT_PATH', __ROOT_PATH + '/outputs/')
+    IS_RUNNING_ON_CLOUD = True if (get_value('IS_RUNNING_ON_CLOUD', False) == 'True') else False
+    if IS_RUNNING_ON_CLOUD:
+        TITANIC_DATA_PATH = get_value('TITANIC_DATA_PATH', None)
+        TITANIC_CSV_DATA_FILE = get_value('TITANIC_CSV_DATA_FILE', None)
+        TITANIC_XLSX_DATA_FILE = get_value('TITANIC_XLSX_DATA_FILE', None)
+        OUTPUT_PATH = get_value('OUTPUT_PATH', None)
+    else:
+        TITANIC_DATA_PATH = __ROOT_PATH + '/datasets/titanic/'
+        TITANIC_CSV_DATA_FILE = TITANIC_DATA_PATH + 'titanic.csv'
+        TITANIC_XLSX_DATA_FILE = TITANIC_DATA_PATH + 'titanic.xlsx'
+        OUTPUT_PATH = __ROOT_PATH + '/outputs/'
 
--- data_analysis_titanic.py
+++ data_analysis_titanic.py
@@ -1,4 +1,6 @@
 import os
+# import boto3
+# from io import StringIO
 
 import pandas as pd
 
@@ -10,11 +12,15 @@ root_path = os.path.dirname(os.path.abspath(__file__))
 def main():
     print("******    Start   ******")
 
-    df = pd.read_csv(r'' + Config.TITANIC_CSV_DATA_FILE)
-    # titanic_df = pd.read_excel(root_path + './datasets/titanic/titanic.xlsx', sheet_name='titanic')
+    if Config.IS_RUNNING_ON_CLOUD:
+        df = pd.read_csv('s3://{}/{}'.format(Config.TITANIC_DATA_PATH, Config.TITANIC_CSV_DATA_FILE))
+    else:
+        df = pd.read_csv(r'' + Config.TITANIC_CSV_DATA_FILE)
+        # df = pd.read_csv(r'' + Config.TITANIC_XLSX_DATA_FILE)
+
     # print("\nInput file Information ----> \n")
     # df.info()
-    # print(titanic_df.head())
+    # print(df.head())
 
     print("\nTop 10 Elders ----> \n")
     print(df[['Name', 'Age']].sort_values('Age', ascending=False).head(10).to_string(index=False))
@@ -35,14 +41,25 @@ def main():
     print("\nNumber of Females survived in 3rd Class ----> \n")
     print(df[(df.Sex == 'female') & (df.Pclass == 3)][['Sex', 'Survived', 'Pclass']].groupby(['Sex', 'Pclass']).count().to_string(index=False))
 
-    print("\n\nWriting data to File ----> \n")
     # out_dir = Config.OUTPUT_PATH.rsplit('/', 1)[0]
     # if not os.path.exists(Config.OUTPUT_PATH):
     #     os.mkdir(Config.OUTPUT_PATH)
-    os.makedirs(Config.OUTPUT_PATH, exist_ok=True)
+
     csv_data = pd.DataFrame()
     csv_data = csv_data.append([df.head(5), df.tail(5)])
-    csv_data.to_csv(Config.OUTPUT_PATH + "result.csv", index=False)
+
+    if Config.IS_RUNNING_ON_CLOUD:
+        print('Writing data to s3://{}/{}'.format(Config.OUTPUT_PATH, 'result.csv'))
+        # csv_data.to_csv('s3://{}/{}'.format(Config.OUTPUT_PATH, 'result.csv'), index=False)
+        csv_data.to_csv('s3://{}/result.csv'.format(Config.OUTPUT_PATH), index=False, line_terminator='\n')
+        # s3_resource = boto3.resource('s3')
+        # csv_buffer = StringIO()
+        # csv_data.to_csv(csv_buffer, index=False)
+        # s3_resource.Object(Config.OUTPUT_PATH, 'result.csv').put(Body=csv_buffer.getvalue())
+    else:
+        print('Writing data to {}'.format(Config.OUTPUT_PATH + "result.csv"))
+        os.makedirs(Config.OUTPUT_PATH, exist_ok=True)
+        csv_data.to_csv(Config.OUTPUT_PATH + "result.csv", index=False, line_terminator='\n')
 
 
 if __name__ == '__main__':
